abb2a3af4dc583636c2ccec773a565b66c174012
TreeWhispererA <139618051+TreeWhispererA@users.noreply.github.com>
2023-07-24 09:46:30 +0900

    1 file modified (#1)

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import './style.css';
 
+const initialState = { text: 'FreeCodeCamp', count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'change-text':
+      return { ...state, text: 'FCC' };
+    case 'increase-count':
+      return { ...state, count: state.count + 1 };
+    default:
+      return state;
+  }
+};
+
 export default function App() {
-  //set the useState getter and setter
-  const [title, setTitle] = useState('FreeCodeCamp');
-  const handleChange = () => {
-    setTitle("Let's Change the world wit our cutting-edge technology");
-    setTimeout(() => {
-      setTitle('FreeCodeCamp');
-    }, 3000);
-  };
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>{title}</h1>
-      <button onClick={handleChange}>Change Title</button>
+    <div>
+      <h1 style={{ textAlign: 'center' }}>{state.text} CodeCamp</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: 'change-text' });
+        }}
+      >
+        Change the text
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: 'increase-count' });
+        }}
+      >
+        Increase Count
+      </button>
+      <h2>{state.count}</h2>
     </div>
   );
 }
