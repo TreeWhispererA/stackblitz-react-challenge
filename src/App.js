0024011725a935e7ec82e8046f74391b0845b9de
TreeWhispererA <139618051+TreeWhispererA@users.noreply.github.com>
2023-07-24 09:20:28 +0900

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
-import "./style.css";
+import React, { useState } from 'react';
+import './style.css';
 
 export default function App() {
+  //set the useState getter and setter
+  const [title, setTitle] = useState('FreeCodeCamp');
+  const handleChange = () => {
+    setTitle("Let's Change the world wit our cutting-edge technology");
+    setTimeout(() => {
+      setTitle('FreeCodeCamp');
+    }, 3000);
+  };
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div style={{ textAlign: 'center' }}>
+      <h1>{title}</h1>
+      <button onClick={handleChange}>Change Title</button>
     </div>
   );
 }
